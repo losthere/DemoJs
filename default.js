7ee7c940a866238736a4e4ac01220514914a7a45
Swati <swatirathaur@gmail.com>
2020-01-20 11:30:32 +0530

    2nd day

--- default.js
+++ default.js
@@ -1,2 +1,20 @@
-document.write("Hello");
+"use strict"
+let x=5;
+x=4.6;
+
+//alert(`value would be ${x}`);
+
+//document.write("Hello");
 //alert('baby step');
+
+function welcome(user="User"){
+   // alert("Welcome "+user);
+}
+welcome();
+
+function orderWhiskey(){
+    let amount = prompt('How many pegs would you like to order',1);
+    //alert(amount);
+    alert(`please pay ${amount*1000} rupayee` );
+}
+
